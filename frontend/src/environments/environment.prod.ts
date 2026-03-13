2f6b4f8a133085d7e326f9cff08e5125f3d60c8a
Anbu-0 <anbusakthiseenivasan@gmail.com>
2026-03-13 23:28:49 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-render-backend.onrender.com',  // Replace with your Render backend URL
+  apiUrl: 'https://farmchainx-2-x3f2.onrender.com/api',  
   name: 'production'
 };
