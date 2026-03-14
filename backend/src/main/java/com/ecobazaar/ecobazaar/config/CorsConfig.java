1e024a9e073a3566931fc18a22bc703608b277d1
Anbu-0 <anbusakthiseenivasan@gmail.com>
2026-03-14 10:16:28 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/ecobazaar/ecobazaar/config/CorsConfig.java
+++ backend/src/main/java/com/ecobazaar/ecobazaar/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 	    registry.addMapping("/**")
-	            .allowedOrigins("http://localhost:4200","https://farm-chain-x.vercel.app")
+	            .allowedOrigins("http://localhost:4200","https://farm-chain-x.vercel.app","https://*.vercel.app")
 	            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 	            .allowedHeaders("Authorization", "Content-Type", "Accept", "X-Requested-With")
 	            .exposedHeaders("Content-Disposition")
